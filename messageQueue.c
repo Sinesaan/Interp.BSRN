0988b51026a4ac32e163f67d8b3c18bb3f941e0f
ariiana01 <104497804+ariiana01@users.noreply.github.com>
2022-06-20 16:34:23 +0200

    Update messageQueue.c

--- messageQueue.c
+++ messageQueue.c
@@ -38,3 +38,95 @@ int main(int argc, char **argv)
   }
 
   sendbuffer.mtype = 1; // Nachrichtentyp festlegen
+//---------------------------------------------Die 2. MQ ------------------------------------------>
+  int mq_key2 = 12342; // Message Queue Key
+  int rc_msgget2;      // Rückgabewert (return code) von msgget2
+  int rc_msgctl2;      // Rückgabewert (return code) von msgctl2
+  int rc_msgrcv2;      // Rückgabewert (return code) von msgrcv2
+  
+  rc_msgget2 = msgget(mq_key2, IPC_CREAT | 0600);
+  if (rc_msgget2 < 0)
+  {
+    printf("Die Warteschlange konnte nicht erstellt werden.\n");
+    perror("msgget");
+    exit(1);
+  }
+  else
+  {
+    printf("Nachrichtenwarteschlange %i mit ID %i ist bereit.\n",
+           mq_key2, rc_msgget2);
+  }
+
+  sendbuffer.mtype = 1; // Nachrichtentyp festlegen
+  
+  //---------------------------------------------3.  MQ erstellen---------------------------------->
+  int mq_key3 = 12343; // Message Queue Key
+  int rc_msgget3;      // Rückgabewert (return code) von msgget3
+  int rc_msgctl3;      // Rückgabewert (return code) von msgctl3
+  int rc_msgrcv3;      // Rückgabewert (return code) von msgrcv3
+  
+  rc_msgget3 = msgget(mq_key3, IPC_CREAT | 0600);
+  if (rc_msgget3 < 0)
+  {
+    printf("Die Warteschlange konnte nicht erstellt werden.\n");
+    perror("msgget");
+    exit(1);
+  }
+  else
+  {
+    printf("Nachrichtenwarteschlange %i mit ID %i ist bereit.\n",
+           mq_key3, rc_msgget3);
+  }
+
+  sendbuffer.mtype = 1; // Nachrichtentyp festlegen
+    //------------------------------Ein Elternprozess erzeugen von Conv-------------------------------->
+  int conv = fork();
+  if (conv > 0)
+  {
+    sendbuffer.mtype = 1; // Nachrichtentyp festlegen
+   
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++)
+    {
+      sendbuffer.mtext[i] = rand() % 51;
+    }
+
+    // Eine Nachricht in die Nachrichtenwarteschlange schreiben
+    // (senden)
+    if (msgsnd(rc_msgget,
+               &sendbuffer,
+               sizeof(sendbuffer.mtext),
+               IPC_NOWAIT) == -1)
+
+    {
+      printf("Das Senden der Nachricht ist fehlgeschlagen.\n");
+      perror("msgsnd");
+      exit(1);
+    }
+    else
+    {
+    //  for (int i = 0; i < 10; i++)
+    //  {
+     //   printf("Geschriebene Nachricht: %i\n", sendbuffer.mtext[i]);        
+     // }
+    }
+    if (msgsnd(rc_msgget2,
+               &sendbuffer,
+               sizeof(sendbuffer.mtext),
+               IPC_NOWAIT) == -1)
+    {
+      printf("Das Senden der Nachricht ist fehlgeschlagen.\n");
+      perror("msgsnd");
+      exit(1);
+    }
+    else
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        // printf("Geschriebene Nachrichten: %i\n", sendbuffer.mtext[i]);
+      }
+  //    printf("\n\n");
+
+      exit(0);
+    }
+  }
