ed3b6c0ffbd6d236011bed21ee08ce09f0476a86
Sine <103943924+Sinesaan@users.noreply.github.com>
2022-06-20 16:21:31 +0200

    Update Pipe.c

--- Pipe.c
+++ Pipe.c
@@ -71,3 +71,89 @@ void main()
     exit(0);
   }
   //<--------------------------------------------------------------------------------------------------
+  //------------------------Kindprozess von stat----------------------------------------------------->
+  close(pipe1[1]);
+  int stat = fork();
+  if (stat == 0)
+  {
+
+    close(pipe1[1]);
+
+    int arr[10];
+    int n = 10;
+    int sum = 0;
+    int mittelwert = 0;
+
+    read(pipe1[0], &n, sizeof(int));
+    read(pipe1[0], arr, sizeof(int) * 10);
+
+    for (int i = 0; i < n; i++)
+    {
+      sum += arr[i];
+      mittelwert = sum / n;
+    }
+    // printf("Das Summe ist: %d\n", sum); // Zur Kontrolle
+    // printf("Mittelwert ist: %d\n", mittelwert);
+    // close(pipe1[0]);
+
+    // Mittelwert und Summe in die Pipe schreiben
+    write(pipe2[1], &sum, sizeof(int));
+    write(pipe2[1], &mittelwert, sizeof(int));
+
+    close(pipe2[1]);
+    exit(0);
+  }
+  //<-----------------------------------------------------------------------------------------------
+  //------------------------------Report Childprozess----------------------------------------------->
+  close(pipe1[1]);
+  int summe;
+  int mittelwert;
+  int report = fork();
+  if (report == 0)
+  {
+    read(pipe2[0], &summe, sizeof(int));
+    read(pipe2[0], &mittelwert, sizeof(int));
+
+    printf("Die empfangene Summe: %d \n", summe);
+    printf("Der empfangene Mittelwert: %d\n", mittelwert);
+    printf("\n");
+
+    close(pipe2[0]);
+  }
+  //<--------------------------------------------------------------------------------------------------
+  //---------------------------------Childprozess von log -------------------------------------------->
+
+  int log = fork();
+  if (log == 0)
+  {
+    int arr[10];
+    int i, n;
+
+    read(pipe3[0], &n, sizeof(int));
+    read(pipe3[0], &arr, sizeof(int) * 10);
+
+    // printf("Die Zahlen in der Datei: %d\n", arr[i]);
+    // printf("\n");
+    close(pipe1[0]);
+
+    //<-------------------------------------------------------------------------------------------
+    //--------------------------Die Zahlen in eine Datei schreiben------------------------------->
+    FILE *fp;
+    fp = fopen("Zufällige_Zahlen_Pipes.txt", "w"); // Datei namens Zufällige_Zahlen_Pipes wird geöffnet
+    if (fp == NULL)
+    {
+      printf("Die Datei konnte nicht gefunden werden. \n");
+    }
+    else
+    {
+      // array Liste schreiben
+      for (i = 0; i < n; i++)
+      {
+        fprintf(fp, " %d: %d\n", i + 1, arr[i]);
+      }
+    }
+    fclose(fp);
+    close(0);
+  }
+  //<----------------------------------------------------------------------------------------------------
+}
